774d224f5d976e2065c877a9b8dbbbb1a4323384
Menooker <myjisgreat@live.cn>
2017-09-23 14:57:20 +1000

    first blood

---
+++ app/src/main/java/com/passsafe/passsafe/FaceActivity.java
@@ -0,0 +1,13 @@
+package com.passsafe.passsafe;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class FaceActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_face);
+    }
+}
